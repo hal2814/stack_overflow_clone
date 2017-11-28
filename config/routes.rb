ce84df8f12088a428cdd0311aa7473f9db4fdada
Nate McGregor and Riki Montgomery <riki@>
2017-11-28 10:37:33 -0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :responses
-  resources :questions
-  resources :votes
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    resources :questions
+    resources :responses
+  end
+
+  resources :questions do
+    resources :responses
+  end
+
 end
